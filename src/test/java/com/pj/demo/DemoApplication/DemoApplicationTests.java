e2965879d35d8851d4962ec7d14c8606d7a931dd
prasanthjj <72882479+prasanthjj@users.noreply.github.com>
2022-08-23 19:23:17 +0530

    Add files via upload

---
+++ src/test/java/com/pj/demo/DemoApplication/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pj.demo.DemoApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
