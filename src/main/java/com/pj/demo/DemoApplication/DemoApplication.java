2ce73e77348b54b026f209a7718f608471ee72f5
prajana <prasanth.janardhanan@gadgeon.com>
2022-08-24 13:18:25 +0530

    updated pom

--- src/main/java/com/pj/demo/DemoApplication/DemoApplication.java
+++ src/main/java/com/pj/demo/DemoApplication/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.pj.demo;
+package com.pj.demo.DemoApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
